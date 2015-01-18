88a16dbbc532642a0fcf46eee600c3f66348b91f
Buttreygoodness <jeff@readypress.com>
2015-01-18 11:52:13 -0700

    Changing gulpfile to exclude sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps');
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
   return gulp.src('./scss/style.scss')
-    .pipe(sourcemaps.init())
     .pipe(sass({ style: 'expanded' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./'))
 });
 
