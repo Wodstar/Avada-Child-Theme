11bac7c8f6ff6649be37c3be23962a812818c6f3
Jay Wiggins <jay@irk.com>
2015-02-19 05:36:04 -0700

    removing modal login functionality for now - login / logout link in menu

--- functions.php
+++ functions.php
@@ -21,3 +21,37 @@ function head_hook() {
 }
 
 add_action('wp_head', 'head_hook');
+
+// this is wrapped in after_setup_theme because it has to happen later than when the functions.php file runs...
+add_action('after_setup_theme', 'eff_with_avada_menu');
+function eff_with_avada_menu() {
+  remove_action( 'wp_head', 'create_avada_menu' );
+  add_action('wp_head', 'create_wodstar_menu');
+	
+  // 'cause avada wraps this in a conditional statement, we're repeating it here...
+  function default_menu_fallback( $args ) {
+		return $null;
+	}
+}
+
+function create_wodstar_menu() {
+  global $main_menu;
+  $main_menu = wp_nav_menu(array(
+      'theme_location'  => 'main_navigation',
+      'depth'        => 5,
+      'container'     => false,
+      'items_wrap'     => '%3$s' . wodstar_login_logout_link_li(),
+      'menu_class'    => 'nav fusion-navbar-nav',
+      'fallback_cb'     => 'default_menu_fallback',
+      // 'fallback_cb'     => 'FusionCoreFrontendWalker::fallback',
+      // 'walker'      => new FusionCoreFrontendWalker(),
+      'echo'         => false
+    ));
+}
+
+function wodstar_login_logout_link_li(){
+  return '<li class="menu-item menu-item-type-custom menu-item-object-custom">' . wp_loginout($_SERVER['REQUEST_URI'],false) . '</li>';
+}
+
+
+
